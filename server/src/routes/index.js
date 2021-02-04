e053182fa68ea07135076e537558774a551c651f
Luis <kenyold1@gmail.com>
2021-02-04 16:40:03 +0100

    probando

--- server/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-//Almacenamos rutas principales de la aplicacion
-const express = require('express');
-const router = express.Router();
-
-router.get('/', async (req, res) => {
-    res.render('index');
-});
-
-module.exports = router;
