b8f5969f645488924b52c3b25bbbfb81351fe010
jaimemadfer <jaimemf07@gmail.com>
2022-05-07 19:21:01 +0200

    Updates

---
+++ precache-manifest.4cddc5cdee414db1f9a4288cd1c8ffef.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a0dcf768934c3783b287c8074e0a3bc",
+    "url": "/TFG-EscrituraCreativa/index.html"
+  },
+  {
+    "revision": "db89c88302884d932d49",
+    "url": "/TFG-EscrituraCreativa/static/css/2.d05ef1bc.chunk.css"
+  },
+  {
+    "revision": "d41cbc4a999446650574",
+    "url": "/TFG-EscrituraCreativa/static/css/main.94165c4a.chunk.css"
+  },
+  {
+    "revision": "db89c88302884d932d49",
+    "url": "/TFG-EscrituraCreativa/static/js/2.30846eef.chunk.js"
+  },
+  {
+    "revision": "892fd5e0d901558c95c0c2cd27d96063",
+    "url": "/TFG-EscrituraCreativa/static/js/2.30846eef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c5872389b194981a9bf",
+    "url": "/TFG-EscrituraCreativa/static/js/3.219045d9.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/TFG-EscrituraCreativa/static/js/3.219045d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f441463cad07410c096b",
+    "url": "/TFG-EscrituraCreativa/static/js/4.d9cdc5d2.chunk.js"
+  },
+  {
+    "revision": "9127c945005b1c05fcb5ed1efd0e0e53",
+    "url": "/TFG-EscrituraCreativa/static/js/4.d9cdc5d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78c664522dc7a1140061",
+    "url": "/TFG-EscrituraCreativa/static/js/5.794d5593.chunk.js"
+  },
+  {
+    "revision": "4c2f580656954a1ad1c84a3bcb195d1c",
+    "url": "/TFG-EscrituraCreativa/static/js/5.794d5593.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d41cbc4a999446650574",
+    "url": "/TFG-EscrituraCreativa/static/js/main.e40fdc41.chunk.js"
+  },
+  {
+    "revision": "e5cf50025238f46381e4",
+    "url": "/TFG-EscrituraCreativa/static/js/runtime-main.485e9a2e.js"
+  }
+]);
